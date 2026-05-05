6f2bee066f33645822998d4ea4d42549f85fa545
Testouri Hichem <hichemtess@gmail.com>
2026-05-05 19:17:46 +0200

    navbar SHOP finished

--- assests/javaScript/main.js
+++ assests/javaScript/main.js
@@ -4,7 +4,22 @@ fetch("../../teams.json")
     const params = new URLSearchParams(window.location.search);
     const teamId = params.get("team");
     const selectedTeam = data.teams.find((t) => t.id === teamId);
-    console.log(teamId);
+    let colorPrimary = selectedTeam.colors.primary;
+    let colorSecondary = selectedTeam.colors.secondary;
+    /*HTML ELEMENTS CALL*/
 
-    let navbarShop = document.querySelector(".shop");
+    let navbarShop = document.querySelector("#main-nav");
+    let secNav = document.querySelector("#secNav");
+    let imgNav = document.querySelector("#logoNav");
+
+    if (selectedTeam) {
+      navbarShop.style.backgroundColor = colorPrimary;
+      secNav.style.backgroundColor = colorSecondary;
+      let teamLogo = selectedTeam.logo;
+      imgNav.src = teamLogo;
+      img.alt = `${selectedTeam.name} logo`;
+    } else {
+      navbarShop.style.backgroundColor = "#000";
+      secNav.style.backgroundColor = "#0000ff";
+    }
   });
