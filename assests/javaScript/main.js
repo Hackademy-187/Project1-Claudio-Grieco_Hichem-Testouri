7c6ce867e90ad449f0642a91b67ddd559ac06b79
Claudio Grieco <claudiogrieco00@icloud.com>
2026-05-06 13:55:45 +0200

    add dark mode

--- assests/javaScript/main.js
+++ assests/javaScript/main.js
@@ -25,3 +25,31 @@ fetch("../../teams.json")
       secNav.style.backgroundColor = "#0000ff";
     }
   });
+// DARK MODE SETTING
+let btnDarkMode = document.querySelector(".btnDarkMode");
+let isClicked = false;
+let whiteNav = document.querySelector(".whiteNav");
+let costumButton = document.querySelector(".costumButton");
+
+btnDarkMode.addEventListener("click", () => {
+  if (isClicked) {
+    document.body.style.backgroundColor = "#000";
+    document.body.style.color = "#fff";
+    document.querySelector(".whiteNav").style.backgroundColor = "#000";
+    document.querySelector(".whiteNav").style.color = "#fff";
+    document.querySelector(".costumButton").style.backgroundColor = "#fff";
+    document.querySelector(".costumButton").style.color = "#000";
+    isClicked = false;
+    btnDarkMode.innerHTML = `<i class="fa-sharp fa-solid fa-sun" style="color: rgb(219, 211, 4);"></i>`;
+  } else {
+    document.body.style.backgroundColor = "#fff";
+    document.body.style.color = "#000";
+    document.querySelector(".whiteNav").style.backgroundColor = "#fff";
+    document.querySelector(".whiteNav").style.color = "#000";
+    document.querySelector(".costumButton").style.backgroundColor = "#000";
+    document.querySelector(".costumButton").style.color = "#fff";
+    isClicked = true;
+    btnDarkMode.innerHTML = `<i class="fa-sharp fa-solid fa-moon" style="color: rgb(255, 255, 255);"></i>`;   
+  }
+});
+// 
